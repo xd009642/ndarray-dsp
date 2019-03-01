a305c6b181d00d071b799c0967a999223f32d1d2
xd009642 <danielmckenna93@gmail.com>
2019-03-01 17:50:23 +0000

    Added inverse fourier transform

--- src/dft.rs
+++ src/dft.rs
@@ -50,6 +50,30 @@ where
     }
 }
 
+impl InverseTransformExt for Dft<Complex<f64>> {
+    type Output = Array2<Complex<f64>>;
+
+    fn inverse(self) -> Self::Output {
+        let inv = self.0.reversed_axes();
+        let shape = inv.shape();
+        let scale = 1.0 / ((shape[0] * shape[1]) as f64);
+
+        let mut planner = FFTplanner::<f64>::new(true);
+        let row_fft = planner.plan_fft(shape[0]);
+        let col_fft = planner.plan_fft(shape[1]);
+
+        let row_freqs = fft_rows(inv.view(), row_fft);
+        let ans = if shape[1] == 1 {
+            row_freqs.reversed_axes()
+        } else {
+            let row_freqs = row_freqs.reversed_axes();
+            fft_rows(row_freqs.view(), col_fft)
+        };
+
+        ans.mapv(|x| x * scale)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,7 +85,7 @@ mod tests {
     }
 
     #[test]
-    fn simple_test() {
+    fn forward_fourier() {
         let expected = arr2(&[
             [c(1.0, 0.0), c(-0.5, -0.8660254), c(-0.5, 0.8660254)],
             [c(-0.5, -0.8660254), c(-0.5, 0.8660254), c(1.0, 0.0)],
@@ -69,9 +93,35 @@ mod tests {
         ]);
 
         let spatial: Array2<f64> = arr2(&[[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]]);
-        let spatial = Dft(spatial);
+        let frequency_domain = Dft(spatial);
 
-        let actual = spatial.transform();
+        let actual = frequency_domain.transform();
+
+        for (a, e) in actual.iter().zip(expected.iter()) {
+            assert_approx_eq!(a.re, e.re);
+            assert_approx_eq!(a.im, e.im);
+        }
+    }
+
+    #[test]
+    fn inverse_fourier() {
+        let zero = c(0.0, 0.0);
+        let one = c(1.0, 0.0);
+        let expected = arr2(&[
+            [one, zero, zero, zero],
+            [zero, zero, zero, one],
+            [zero, zero, one, zero],
+            [zero, one, zero, zero],
+        ]);
+
+        let freq = arr2(&[
+            [c(4.0, 0.0), zero, zero, zero],
+            [zero, c(4.0, 0.0), zero, zero],
+            [zero, zero, c(4.0, 0.0), zero],
+            [zero, zero, zero, c(4.0, 0.0)],
+        ]);
+        let freq = Dft(freq);
+        let actual = freq.inverse();
 
         for (a, e) in actual.iter().zip(expected.iter()) {
             assert_approx_eq!(a.re, e.re);
